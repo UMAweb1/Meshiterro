06f5eff70a9990e100561c13f0bfe061b9b1cc4a
yuma-oji <yumhouse.a@gmail.com>
2019-10-08 16:53:40 +0900

    [5-3]Meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '91536d18664e8516477ce7c47d120135eb5f69c293d94963bdfd7fb2658318aa9d5535ac3f4273cb4e77ccf6765abaa3107a95a52a4542c81a4cab25f2601ed5'
