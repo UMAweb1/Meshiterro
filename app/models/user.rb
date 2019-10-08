c821e977806a145d27c9af26b18e869e94d8b49b
yuma-oji <yumhouse.a@gmail.com>
2019-10-08 21:11:43 +0900

    [5-5]Meshiterro

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  # いいね機能
+  has_many :favorites, dependent: :destroy
 end
